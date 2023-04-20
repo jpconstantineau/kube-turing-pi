6c6c5b8344ee8dc3a09cb990a8d4093329d4e602
Pierre Constantineau <jpconstantineau@gmail.com>
2023-04-20 17:27:03 -0600

    updated script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,16 @@
 
 curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644"  INSTALL_K3S_VERSION="v1.27.1-rc1+k3s1" INSTALL_K3S_EXEC="--flannel-backend=none --disable-network-policy --cluster-cidr=192.168.0.0/16" sh -
-watch kubectl get pods -A -o wide
+export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
+sleep 15
+kubectl get nodes -A -o wide
 kubectl apply -f gitops/crd/kube-system/calico.yaml
-watch kubectl get pods -A -o wide
+sleep 15
+kubectl get pods -A -o wide
 kubectl create namespace argocd
 kubectl apply -n argocd -f gitops/crd/argocd/install.yaml
 kubectl apply -n argocd -f gitops/bootstrap.yaml
-watch kubectl get pods -A -o wide
+kubectl get pods -A -o wide
+sleep 30
 kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d > admin-pass.txt
 cat admin-pass.txt
 echo login to argocd.192-168-10-74.nip.io with the password above
